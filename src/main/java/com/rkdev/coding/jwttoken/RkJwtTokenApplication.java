b7c3afa2c4f94e71d3b76327c27d18fad5d29f51
Rajesh Kapa <rajesh.tist@gmail.com>
2022-06-26 12:46:28 -0500

    Initial commit

---
+++ src/main/java/com/rkdev/coding/jwttoken/RkJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.rkdev.coding.jwttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkJwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkdev/coding/jwttoken/RkJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkdev.coding.jwttoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkJwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
